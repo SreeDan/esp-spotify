8ae2173ae2482e21c51a4e24bb0a64fc0dcc89bc
sreekara <sreedansd@gmail.com>
2024-06-22 21:20:10 -0400

    feat(graphics): added ability to resize and convert images from RGB888 to RGB565

--- graphics/src/lib.rs
+++ graphics/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+fn rgb888_to_rgb565(r: u8, g: u8, b: u8) -> u16 {
+    let red = (r >> 3) as u16;
+    let green = (g >> 2) as u16;
+    let blue = (b >> 3) as u16;
+
+    (red << 11) | (green << 5) | blue
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn convert_vec_rgb888_to_rgb565(rgb888_vec: &Vec<u8>) -> Vec<u8> {
+    let mut rgb565_vec = Vec::with_capacity((rgb888_vec.len() / 3) * 2);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    for chunk in rgb888_vec.chunks_exact(3) {
+        let r = chunk[0];
+        let g = chunk[1];
+        let b = chunk[2];
+        let rgb565 = rgb888_to_rgb565(r, g, b);
+        rgb565_vec.push((rgb565 >> 8) as u8); // High byte
+        rgb565_vec.push(rgb565 as u8); // Low byte
     }
+
+    rgb565_vec
 }
+
--- graphics_sim/src/main.rs
+++ graphics_sim/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use embedded_graphics::{image::ImageRaw, pixelcolor::Rgb565, prelude::*};
+use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
+use image::io::Reader as ImageReader;
+use image::{DynamicImage, RgbImage};
+use reqwest::blocking::get;
+use std::io::Cursor;
+
+fn main() -> Result<(), core::convert::Infallible> {
+    let mut display = SimulatorDisplay::<Rgb565>::new(Size::new(240, 320));
+
+    // URL of the Spotify album image
+    let url = "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228";
+
+    // Fetch and decode the image
+    let image_bytes = get(url).ok().unwrap().bytes().ok().unwrap();
+
+    let img = ImageReader::new(Cursor::new(image_bytes))
+        .with_guessed_format()
+        .expect("Failed to guess image format")
+        .decode()
+        .expect("Failed to decode image");
+
+    let rgb_imag: RgbImage = img.into_rgb8();
+    let resized_image = DynamicImage::ImageRgb8(rgb_imag)
+        .resize(150, 150, image::imageops::FilterType::Nearest)
+        .to_rgb8();
+
+    let raw = resized_image.clone().into_raw();
+    let rgb565: Vec<u8> = graphics::convert_vec_rgb888_to_rgb565(&raw);
+
+    let out: ImageRaw<Rgb565> = ImageRaw::new(&rgb565, resized_image.width());
+    out.draw(&mut display)?;
+
+    let output_settings = OutputSettingsBuilder::new().scale(2).build();
+    Window::new("Hello world", &output_settings).show_static(&display);
+
+    Ok(())
 }
